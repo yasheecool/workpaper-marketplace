398411d1d3d6b8ce3933dbb849b8039891207e54
Yash Talreja <yasheecool@gmail.com>
2026-01-22 12:07:42 +1000

    fix proxy: add proper redirects

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,32 +2,36 @@ import { type NextRequest } from 'next/server';
 import { updateSession } from '@/lib/supabase/proxy';
 import { NextResponse } from 'next/server';
 
-const openUrls = ['/login', '/auth', '/', '/listing', '/vendor-details'];
+//including / in public urls will cause a bug because all urls start with /
+const publicUrls = ['/login', '/auth', '/listing', '/vendor-details'];
 
 export async function proxy(request: NextRequest) {
   const { supabaseResponse, userClaims } = await updateSession(request);
   const selectedFirm = request.cookies.get('selected_firm_id')?.value;
 
+  //If no user session found, and url is not homepage or public url, redirect to login
   if (
     !userClaims &&
-    !openUrls.some((url) => request.nextUrl.pathname.startsWith(url))
+    request.nextUrl.pathname !== '/' &&
+    !publicUrls.some((url) => request.nextUrl.pathname.startsWith(url))
   ) {
     console.log('No user session found, will redirect to login');
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = '/login';
     return NextResponse.redirect(url);
-  } else console.log('User logged in!');
+  }
 
+  //If user is logged in but has not selected a firm, redirect to firm selection page
   if (
+    userClaims &&
     !selectedFirm &&
-    !request.nextUrl.pathname.startsWith('/firm-selection')
+    request.nextUrl.pathname !== '/firm-selection'
   ) {
     console.log('No firm selected');
-    // no firm selected, redirect to firm selection page
     const url = request.nextUrl.clone();
     url.pathname = '/firm-selection';
-    // return NextResponse.redirect(url);
+    return NextResponse.redirect(url);
   }
 
   return supabaseResponse;
